eb33f1c74e2e94455ef3734b4ab99ea134ccf673
Fuyuki Malahom <fuyuki325@gmail.com>
2024-12-26 22:33:13 +0900

    container made

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,6 +1,8 @@
 import type { Config } from "tailwindcss";
+import colors from "tailwindcss/colors";
 
 export default {
+  darkMode: ["class"],
   content: [
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
@@ -9,10 +11,165 @@ export default {
   theme: {
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        primary: {
+          "50": "#fcf3f6",
+          "100": "#fbe8f0",
+          "200": "#f8d2e1",
+          "300": "#f4adc8",
+          "400": "#ef94b4",
+          "500": "#e15381",
+          "600": "#cf335d",
+          "700": "#b32346",
+          "800": "#94203b",
+          "900": "#7c1f34",
+        },
+        secondary: {
+          "50": "#f0f8ff",
+          "100": "#e0f0fe",
+          "200": "#bae2fd",
+          "300": "#7ccbfd",
+          "400": "#37b1f9",
+          "500": "#0d97ea",
+          "600": "#016fb9",
+          "700": "#025fa2",
+          "800": "#065186",
+          "900": "#0c446e",
+        },
+        white: "#FFFFFF",
+        black: "#1f1f1f",
+        slate: colors.slate,
+        gray: colors.gray,
+
+        neutral: colors.neutral,
+        red: colors.red,
+        green: colors.green,
+        transparent: "transparent",
+
+        heading: colors.black,
+        paragraph: colors.gray[600],
+        label: colors.black,
+        placeholder: colors.gray[400],
+        body: colors.slate[700],
+
+        table: colors.gray[900],
+        background: colors.gray[100],
+
+        border: colors.gray[200],
+        "inputs-border": colors.gray[300],
+        icon: colors.gray[500],
+      },
+      fontSize: {
+        sm: "0.8rem",
+        base: "1rem",
+        xl: "1.25rem",
+        "2xl": "1.563rem",
+        "3xl": "1.953rem",
+        "4xl": "2.441rem",
+        "5xl": "3.052rem",
+
+        // custom font size
+        h1: [
+          "4rem",
+          {
+            lineHeight: "5rem",
+            letterSpacing: "0.0375em",
+            fontWeight: "700",
+          },
+        ],
+        h2: [
+          "3rem",
+          {
+            lineHeight: "2rem",
+            letterSpacing: "0.035em",
+            fontWeight: "700",
+          },
+        ],
+        h3: [
+          "2.5rem",
+          {
+            lineHeight: "3.5rem",
+            letterSpacing: "0.05em",
+            fontWeight: "700",
+          },
+        ],
+        h4: [
+          "2rem",
+          {
+            lineHeight: "2.75rem",
+            letterSpacing: "0em",
+            fontWeight: "700",
+          },
+        ],
+        h5: [
+          "1.5rem",
+          {
+            lineHeight: "2.5rem",
+            letterSpacing: "0em",
+            fontWeight: "600",
+          },
+        ],
+        h6: [
+          "1.25rem",
+          {
+            lineHeight: "2rem",
+            letterSpacing: "0rem",
+            fontWeight: "500",
+          },
+        ],
+
+        p: [
+          "0.875rem",
+          {
+            lineHeight: "1.5rem",
+            letterSpacing: "0rem",
+            fontWeight: "400",
+          },
+        ],
+        body: [
+          "1rem",
+          {
+            lineHeight: "1.5rem",
+            letterSpacing: "0rem",
+            fontWeight: "400",
+          },
+        ],
+
+        span: [
+          "0.75rem",
+          {
+            lineHeight: "1.5rem",
+            letterSpacing: "0rem",
+            fontWeight: "400",
+          },
+        ],
+      },
+      container: {
+				center: true,
+        screens: {
+          sm: "640px",
+          // => @media (max-width: 640px) { ... }
+
+          md: "768px",
+          // => @media (max-width: 768px) { ... }
+
+          lg: "1024px",
+          // => @media (max-width: 1024px) { ... }
+
+          xl: "1280px",
+          // => @media (max-width: 1280px) { ... }
+
+          "2xl": "1536px",
+          // => @media (max-width: 1536px) { ... }
+        },
+				padding: {
+					DEFAULT: '1rem',
+					sm: '2rem',
+					lg: '4rem',
+					xl: '5rem',
+					'2xl': '6rem',
+				},
       },
     },
   },
-  plugins: [],
+  plugins: [require("tailwindcss-animate")],
 } satisfies Config;
